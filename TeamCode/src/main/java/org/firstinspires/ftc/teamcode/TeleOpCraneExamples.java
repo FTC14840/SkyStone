0856d37621b25d1496fdee33f5cb084b19ef289c
ftc14840 <ftc14840@decaturchristian.net>
2019-12-27 14:25:13 -0600

    TFOD Example

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCraneExamples.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCraneExamples.java
@@ -9,16 +9,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class TeleOpCraneExamples extends LinearOpMode {
 
-    private DcMotor craneLeft = null;
-    private DcMotor craneRight = null;
+    private DcMotor craneLift = null;
     private DcMotor craneExtension = null;
-    private DcMotor headCompressor = null;
 
     private Servo hookLeft = null;
     private Servo hookRight = null;
     private Servo armLeft = null;
     private Servo armRight = null;
     private Servo headSwivel = null;
+    private Servo headCompressor = null;
     private Servo headRotate = null;
 
 
@@ -27,26 +26,13 @@ public class TeleOpCraneExamples extends LinearOpMode {
 
         //------------------------------------------------------------------------------------------
 
-        // This will change once worm gear is installed
-        craneLeft = hardwareMap.dcMotor.get("craneLeft");
-        craneRight = hardwareMap.dcMotor.get("craneRight");
-        craneLeft.setDirection(DcMotor.Direction.REVERSE);
+        craneLift = hardwareMap.dcMotor.get("craneLift");
 
         //------------------------------------------------------------------------------------------
 
-        // We need to purchase and install a Tetrix 20:1 DC Motor
         craneExtension = hardwareMap.dcMotor.get("craneExtension");
-        //craneExtension.setDirection(DcMotor.Direction.REVERSE);
         craneExtension.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         craneExtension.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        //------------------------------------------------------------------------------------------
-
-        // We need to purchase and install a Tetrix 20:1 DC Motor
-        headCompressor = hardwareMap.dcMotor.get("headCompressor");
-        //headCompressor.setDirection(DcMotor.Direction.REVERSE);
-        headCompressor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        headCompressor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         //------------------------------------------------------------------------------------------
 
@@ -61,8 +47,6 @@ public class TeleOpCraneExamples extends LinearOpMode {
         armLeft = hardwareMap.servo.get("armLeft");
         armRight = hardwareMap.servo.get("armRight");
 
-        boolean armMode = false;
-
         double armLeftPosition = 1.0;
         armLeft.setPosition(armLeftPosition);
         double armRightPosition = 0.0;
@@ -72,6 +56,12 @@ public class TeleOpCraneExamples extends LinearOpMode {
 
         headSwivel = hardwareMap.servo.get("headSwivel");
         headSwivel.setPosition(.46);
+
+        //------------------------------------------------------------------------------------------
+
+        headCompressor = hardwareMap.servo.get("headCompressor");
+        double headCompressorPosition = 0.5;
+        headCompressor.setPosition(headCompressorPosition);
 
         //------------------------------------------------------------------------------------------
 
@@ -92,12 +82,11 @@ public class TeleOpCraneExamples extends LinearOpMode {
 
             //--------------------------------------------------------------------------------------
 
-            craneLeft.setPower(gamepad2.left_stick_y * .50);
-            craneRight.setPower(gamepad2.left_stick_y * .50);
+            craneLift.setPower(gamepad2.left_stick_y);
 
             //--------------------------------------------------------------------------------------
 
-            int craneExtensionMax = 11500;
+            int craneExtensionMax = -10500;
             int craneExtensionMin = 0;
 
             if (gamepad2.right_bumper) {
@@ -126,70 +115,7 @@ public class TeleOpCraneExamples extends LinearOpMode {
             }
 
             // Do not use this in competition, for setup only
-            craneExtension.setPower(gamepad1.left_stick_y);
-            DisplayTelemetry();
-
-            //--------------------------------------------------------------------------------------
-
-            int headCompressorMaxLong = 10000;
-            int headCompressorMinLong = 0;
-            int headCompressorMaxShort = 5000;
-            int headCompressorMinShort = 0;
-
-            if (gamepad2.a) {
-
-                headCompressor.setTargetPosition(headCompressorMaxLong);
-                headCompressor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                headCompressor.setPower(1);
-                while (headCompressor.isBusy()) {
-                    DisplayTelemetry();
-                }
-                headCompressor.setPower(0);
-                headCompressor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-            }
-
-            if (gamepad2.b) {
-
-                headCompressor.setTargetPosition(headCompressorMinLong);
-                headCompressor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                headCompressor.setPower(1);
-                while (headCompressor.isBusy()) {
-                    DisplayTelemetry();
-                }
-                headCompressor.setPower(0);
-                headCompressor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-            }
-
-            if (gamepad2.x) {
-
-                headCompressor.setTargetPosition(headCompressorMaxShort);
-                headCompressor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                headCompressor.setPower(1);
-                while (headCompressor.isBusy()) {
-                    DisplayTelemetry();
-                }
-                headCompressor.setPower(0);
-                headCompressor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-            }
-
-            if (gamepad2.y) {
-
-                headCompressor.setTargetPosition(headCompressorMinShort);
-                headCompressor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                headCompressor.setPower(1);
-                while (headCompressor.isBusy()) {
-                    DisplayTelemetry();
-                }
-                headCompressor.setPower(0);
-                headCompressor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-            }
-
-            // Do not use this in competition, for setup only
-            headCompressor.setPower(gamepad1.right_stick_y);
+            craneExtension.setPower(gamepad1.right_stick_y);
             DisplayTelemetry();
 
             //--------------------------------------------------------------------------------------
@@ -211,7 +137,7 @@ public class TeleOpCraneExamples extends LinearOpMode {
                 armRightPosition = .50;
             }
 
-            if (gamepad2.left_stick_button){
+            if (gamepad2.left_stick_button) {
                 armLeftPosition = 1.0;
                 armRightPosition = 0.0;
             }
@@ -235,20 +161,30 @@ public class TeleOpCraneExamples extends LinearOpMode {
 
             //--------------------------------------------------------------------------------------
 
-            headRotate.setPosition(headRotatePosition + gamepad2.right_stick_x);
+            if (gamepad2.x) {
+                headCompressor.setPosition(headCompressorPosition -= 1.0);
+            }
+
+            if (gamepad2.a) {
+                headCompressor.setPosition(headCompressorPosition = 0.5);
+            }
+
+            if (gamepad2.b) {
+                headCompressor.setPosition(headCompressorPosition += 1.0);
+            }
 
             //--------------------------------------------------------------------------------------
 
+            headRotate.setPosition(headRotatePosition + gamepad2.right_stick_x);
+
+            //--------------------------------------------------------------------------------------
         }
     }
 
-    public void DisplayTelemetry() {
+    public void DisplayTelemetry () {
         telemetry.addLine()
                 .addData("Crane Extension", "%7d" + "   " + "%7d" + "   " + "%.1f",
                         craneExtension.getTargetPosition(), craneExtension.getCurrentPosition(), craneExtension.getPower());
-        telemetry.addLine()
-                .addData("Head Compressor", "%7d" + "   " + "%7d" + "   " + "%.1f",
-                        headCompressor.getTargetPosition(), headCompressor.getCurrentPosition(), headCompressor.getPower());
         telemetry.update();
     }
 }
